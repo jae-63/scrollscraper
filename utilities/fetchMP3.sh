6c9ebdc844412a3f10d4773626d3667e53bd3600
Jonathan Epstein <JonathanEpstein9@gmail.com>
2023-07-05 21:49:58 -0400

    Create fetchMP3.sh
    script to fetch large MP3 tarballs from personal Google Drive

---
+++ utilities/fetchMP3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+declare -A newmap
+newmap[t1]="1SFv3MOqujXB7ZzNKV6Jm2EgE_xRvAsli"
+newmap[t2]="17HaUXIylErTJDI0ow7q-j25CfUbD8ViC"
+newmap[t3]="1nQQbdpwkIh7eCv0-Zohfnyk-5Ev7zCNz"
+newmap[t4]="1Oda5lZ8iXJ_Ut1DdoS_wwfOqmnAsZeKw"
+newmap[t5]="1rQPS8hbDz3cvFprSaqY7sX10vA79EskM"
+
+#gdown https://drive.google.com/uc?id=${newmap[t5]}
+for tname in t1 t2 t3 t4 t4; do
+  outname="ORT_MP3s.recoded.${tname}.tar"
+  echo gdown --id ${newmap[t5]} -O data/${outname}
+done
