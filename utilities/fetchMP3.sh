d0006543cd40f93cf465cfc18004447436214fa1
Jonathan Epstein <JonathanEpstein9@gmail.com>
2023-07-05 21:57:08 -0400

    Update fetchMP3.sh
    typo

--- utilities/fetchMP3.sh
+++ utilities/fetchMP3.sh
@@ -7,7 +7,7 @@ newmap[t4]="1Oda5lZ8iXJ_Ut1DdoS_wwfOqmnAsZeKw"
 newmap[t5]="1rQPS8hbDz3cvFprSaqY7sX10vA79EskM"
 
 #gdown https://drive.google.com/uc?id=${newmap[t5]}
-for tname in t1 t2 t3 t4 t4; do
+for tname in t1 t2 t3 t4 t5; do
   outname="ORT_MP3s.recoded.${tname}.tar"
   gdown --id ${newmap[t5]} -O data/${outname}
   (cd data; mkdir -p ORT_MP3s.recoded/$tname; cd ORT_MP3s.recoded/$tname; tar xf ../../${outname})
