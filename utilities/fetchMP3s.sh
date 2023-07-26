9f09f1ab04068ece0dcc22f4a925e9665575f65c
jae-63 <JonathanEpstein9@gmail.com>
2023-07-26 08:46:34 -0400

    switch to smaller tarballs without Alexa skill concatenated MP3s.  Bug fix to download different tarballs for the five books

--- utilities/fetchMP3s.sh
+++ utilities/fetchMP3s.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 declare -A newmap
-newmap[t1]="1SFv3MOqujXB7ZzNKV6Jm2EgE_xRvAsli"
-newmap[t2]="17HaUXIylErTJDI0ow7q-j25CfUbD8ViC"
-newmap[t3]="1nQQbdpwkIh7eCv0-Zohfnyk-5Ev7zCNz"
-newmap[t4]="1Oda5lZ8iXJ_Ut1DdoS_wwfOqmnAsZeKw"
-newmap[t5]="1rQPS8hbDz3cvFprSaqY7sX10vA79EskM"
+
+# https://drive.google.com/file/d/13AR-eT3WrQSZ7noO9b6GEiqqXL8a5awz/view?usp=sharing
+newmap[t1]="13AR-eT3WrQSZ7noO9b6GEiqqXL8a5awz"
+# https://drive.google.com/file/d/1zOn02n4DIySBBdYyr5RXKRjZzLe8UngI/view?usp=sharing
+newmap[t2]="1zOn02n4DIySBBdYyr5RXKRjZzLe8UngI"
+# https://drive.google.com/file/d/1ditZtFKx_ti6I7SN1_hQxIHecx0EkaIA/view?usp=sharing
+newmap[t3]="1ditZtFKx_ti6I7SN1_hQxIHecx0EkaIA"
+# https://drive.google.com/file/d/1fyv3a7eIhzB7zGDleXp5yDzoS51YS4GL/view?usp=sharing
+newmap[t4]="1fyv3a7eIhzB7zGDleXp5yDzoS51YS4GL"
+# https://drive.google.com/file/d/1q4RMYvOBm_t1AeQ_r6tpjMweMu4Cf_yx/view?usp=sharing
+newmap[t5]="1q4RMYvOBm_t1AeQ_r6tpjMweMu4Cf_yx"
 
 OUTDIR=/ort_mp3s
 if [ -z "$IS_DOCKER" ]; then
@@ -15,7 +21,7 @@ STARTDIR=$(pwd)
 mkdir -p $STARTDIR/data
 #gdown https://drive.google.com/uc?id=${newmap[t5]}
 for tname in t1 t2 t3 t4 t5; do
-  outname="ORT_MP3s.recoded.${tname}.tar"
-  gdown --id ${newmap[t5]} -O ${STARTDIR}/data/${outname}
-  (cd ${STARTDIR}/data; mkdir -p ${OUTDIR}/$tname; cd ${OUTDIR}/$tname; tar xf ${STARTDIR}/data/${outname})
+  outname="ORT_MP3s.recoded.${tname}.tgz"
+  gdown --id ${newmap[$tname]} -O ${STARTDIR}/data/${outname}
+  (cd ${STARTDIR}/data; mkdir -p ${OUTDIR}/$tname; cd ${OUTDIR}/$tname; tar xzf ${STARTDIR}/data/${outname})
 done
